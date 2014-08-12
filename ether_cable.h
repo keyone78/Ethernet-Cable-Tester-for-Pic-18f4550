876ecf02ccfd5eade523e43fb7b4415445f8975d
keyone78 <lewkeewan@gamil.com>
2014-08-12 21:06:36 +0800

    Create ether_cable.h

---
+++ ether_cable.h
@@ -0,0 +1,16 @@
+// ether_cable.h
+
+
+unsigned char str_Atest_con(void);
+
+void flash_etherA(void);
+
+void entherA_low(void);
+
+
+unsigned char single_modeTest(unsigned char );
+
+void pinToHigh_Test(unsigned char num);
+
+
+unsigned char connect_error(unsigned char seq_no,unsigned char testno);
